36c3e41840f69cff187c81ead4cc43ddda4c3923
gouthamiMotivity <100825829+gouthamiMotivity@users.noreply.github.com>
2023-06-06 16:55:44 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,21 @@
+//Nested html elements creation 
+
+const parent= React.createElement("div",{id:"parent"}, 
+React.createElement("div",{id:"child"},[
+    React.createElement("h1",{id:"heading"},"I am heading tag"),
+    React.createElement("h2",{id:"heading"},"I am heading tag")
+]),
+React.createElement("div",{id:"child2"},[
+    React.createElement("h1",{id:"heading"},"I am heading2 tag"),
+    React.createElement("h2",{id:"heading"},"I am heading2 tag")
+]));
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+console.log(parent)
+
+//Single element
+
+// const heading=React.createElement("h1",{id:"heading"},"hello");
+// const root=ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading)
+// console.log(heading)
