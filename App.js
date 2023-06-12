4012d0256f5d28dc6a71b133cd46373aceb3f50f
Gouthami-D <gouthamidubbaka98@gmail.com>
2023-06-12 16:52:19 +0530

    Add notes.txt

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
 //Nested html elements creation 
 
-const parent= React.createElement("div",{id:"parent"}, 
-React.createElement("div",{id:"child"},[
-    React.createElement("h1",{id:"heading"},"I am heading tag"),
-    React.createElement("h2",{id:"heading"},"I am heading tag")
-]),
-React.createElement("div",{id:"child2"},[
-    React.createElement("h1",{id:"heading"},"I am heading2 tag"),
-    React.createElement("h2",{id:"heading"},"I am heading2 tag")
-]));
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
-console.log(parent)
+// const parent= React.createElement("div",{id:"parent"}, 
+// React.createElement("div",{id:"child"},[
+//     React.createElement("h1",{id:"heading"},"I am heading tag"),
+//     React.createElement("h2",{id:"heading"},"I am heading tag")
+// ]),
+// React.createElement("div",{id:"child2"},[
+//     React.createElement("h1",{id:"heading"},"I am heading2 tag"),
+//     React.createElement("h2",{id:"heading"},"I am heading2 tag")
+// ]));
+// const root=ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
+// console.log(parent)
 
 //Single element
 
@@ -19,3 +22,8 @@ console.log(parent)
 // const root=ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading)
 // console.log(heading)
+
+const heading=React.createElement("h1",{id:"heading"},"This is reactjs");
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading)
+console.log(heading)
