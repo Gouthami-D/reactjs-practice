986b261363296195f7686d0586056fcac4b0b61f
Gouthami-D <gouthamidubbaka98@gmail.com>
2023-06-15 16:18:28 +0530

    Foodcards with API

--- App.js
+++ App.js
@@ -43,18 +43,24 @@ const Header =()=>{
 
 <div className="container">
 <div className="row">
-<FoodCards resData={resList[0]}/>
+{/* <FoodCards resData={resList[0]}/>
 <FoodCards resData={resList[1]}/>
 <FoodCards resData={resList[2]}/>
 <FoodCards resData={resList[3]}/>
 <FoodCards resData={resList[4]}/>
 <FoodCards resData={resList[5]}/>
 <FoodCards resData={resList[6]}/>
-<FoodCards resData={resList[7]}/>
+<FoodCards resData={resList[7]}/> */}
+ {resList.map((restaurantcard)=>(
+   <FoodCards key={restaurantcard.data.id} resData={restaurantcard}/>
+ ))
+ }
+  
 </div>
 </div>
 </>
     )
+    
 }
 const resList=[
     {
@@ -1641,23 +1647,25 @@ const FoodCards = (props) => {
     const {resData}= props;
     console.log(props)
     return (
-      <div class="explore-menu-section col-sm-12 col-md-3">
-      <div class="container">
-          <div class="row">
-              <div class="">
-                  <div class="shadow menu-item-card p-3 mb-3">
-                      <img src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/"+resData.data.cloudinaryImageId} class="image-card img-fluid w-100" />
+      <div className="explore-menu-section col-sm-12 col-md-3">
+      <div className="container">
+          <div className="row">
+              <div className="">
+                  <div className="shadow menu-item-card p-3 mb-3">
+                      <img src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/"+resData.data.cloudinaryImageId} className="image-card img-fluid w-100" />
                       <h2 className="restaurant-name">{resData.data.name}</h2>
-                      {/* <h1 class="menu-title">{resObj.data.}</h1> */}
-                      <h1 class="cuisine">{resData.data.cuisines.join(", ")}</h1>
+                      {/* <h1 className="menu-title">{resObj.data.}</h1> */}
+                      <h1 className="cuisine">{resData.data.cuisines.join(", ")}</h1>
+                      <h6 className="restaurant-name">{resData.data.costForTwoString.toLowerCase()}</h6>
                             <div className="rating d-flex align-items-baseline">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-star" viewBox="0 0 16 16">
                                     <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
                                 </svg>
+                                
                             <h6 className="m-1">{resData.data.avgRating} rating</h6>
                     </div>
-                      <a class="menu-item-link">View All</a>
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" fill="#d0b200" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+                      <a className="menu-item-link">View All</a>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" fill="#d0b200" className="bi bi-arrow-right-short" viewBox="0 0 16 16">
                           <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
                       </svg>
               </div>
